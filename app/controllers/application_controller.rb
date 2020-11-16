9bf0912217abd28a8bdeabacdab3da4bead2fdde
Martin-Alexander <martingianna@gmail.com>
2020-11-16 10:14:25 -0500

    setup devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user! # `authenticate_user!` it redirects to the sign up page if the user is not signed in
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
